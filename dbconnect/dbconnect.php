42d3277d491d84d93eae89f50e1cc932db6997e4
pluthin <pontus.lut@gmail.com>
2019-03-28 11:44:52 +0100

    added a dbconnect file

---
+++ dbconnect/dbconnect.php
@@ -0,0 +1,22 @@
+
+<?php
+
+class DBConnect {
+    private $host = "localhost";
+    private $database   = "";
+    private $username = "";
+    private $password = "";
+    private $charset = "utf8mb4";
+    public $pdo;
+
+    public function __construct() {
+        $dsn = "mysql:host=$this->host;dbname=$this->database;charset=$this->charset";
+
+        try {
+            $this->pdo = new PDO($dsn, $this->username, $this->password);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+
+}?>
