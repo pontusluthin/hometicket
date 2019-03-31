5207dbca738864961eaa15f5b7a8d8df12a7f91d
pluthin <pontus.lut@gmail.com>
2019-03-31 17:48:15 +0200

    added a function to read all the event data from database

---
+++ event-CRUD/Events.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once('../dbconnect/dbconnect.php');
+
+class Events{
+
+    //connection to database
+        public function __construct()
+        {
+            $db = new DBConnect();
+            $this->db = $db->pdo;
+        }
+
+    //select all data from the database 
+
+        public function select(){
+
+            $sql = "SELECT * FROM Events";
+            $result = $this->db->prepare($sql);
+            $result->execute(); 
+
+                if($result->rowCount() > 0){
+                    
+                        while($row = $result->fetch()){
+                            
+                            $data[] = $row; 
+
+                        }
+
+                        return $data; 
+
+                }
+            
+        }
+
+} 
