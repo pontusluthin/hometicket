e0df89648acad7fda48ea89dda5234e75addd4a9
pluthin <pontus.lut@gmail.com>
2019-04-16 21:03:26 +0200

    edited some search

--- showTicket/Search.php
+++ showTicket/Search.php
@@ -39,7 +39,7 @@ public function searchResult(){
         }
 
         return $data; 
-
+        
         
     }
 }
--- showTicket/showTicket.php
+++ showTicket/showTicket.php
@@ -246,7 +246,7 @@ include 'Search.php';
                                                         </tr>
 
                                                         <?php
-
+                                                        
                                                         }
                                                         ?>
                                     
