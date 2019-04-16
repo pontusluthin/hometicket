26c389b717314e5ff5a080496693e76cb3c7d883
pluthin <pontus.lut@gmail.com>
2019-04-16 20:47:56 +0200

    Fixed some comments on main.js file

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 
+//Beginning of a cookie cart, user can add event tickets to cart but not proceed to checkout and order.
+
 window.onload = function(){ 
 
   let cart = [];
   let cartDisplay = [];
-  console.log(cartDisplay);
   
   
- 
-  /*let addedCartItem = document.createElement("DIV");*/
-  // läs in från cookie
+  //Button click event on every loaded event
   let buyButton = document.querySelectorAll(".buyButton");
       for (var i = 0; i < buyButton.length; i++) {
           var self = buyButton[i];
@@ -21,44 +20,28 @@ window.onload = function(){
             let id = parseInt(this.id.substring(10));//syftar till this button som är klickad att den då hämtar den knappens data. 
            /* console.log(id);*/
        
-                  
+            //Getting data from database      
             let json = JSON.parse(document.getElementById("json_data").innerHTML);
          
             
 
             for (let i = 0; i < json.length; i++) {
               if(json[i]["eventId"] == id) {
-
-               
                 
                 let title = json[i]["eventTitle"];
                 let price = json[i]["eventPrice"];
-
-               /* console.log(title);
-                console.log(price);*/
-
-                
-
                
-
                 let cartitem = {
                   'title': title,
                   'price': price
                 }; 
-
-              
                 
                  cart.push(cartitem);
                 
-                
-                 
-                /*let cookievalue = document.cookie =  JSON.stringify(cart);*/
-
                 let titleItem = cartitem['title'];
                 let priceItem =  cartitem['price'];
 
-               
-
+                //Function that stores clicked event with title + price inb cart array
                 let itemArray = titleItem + priceItem;
                   cartDisplay.push(itemArray); 
                 let putIntoCart = "title="+ cartDisplay;
@@ -67,33 +50,19 @@ window.onload = function(){
   
                 }
                
-              
             }
-
-          
 
         }, false);
        
       }
 
       document.getElementById("cartBtn").addEventListener('click', function (event){
-        
-        
-
-
 
       });
 
   $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
-
-
-
-  
-     
-
-      
 
 };
 
